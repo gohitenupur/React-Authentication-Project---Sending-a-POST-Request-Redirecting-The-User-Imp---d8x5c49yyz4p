dcf24eeb09527a79d6e8d1880fb57bd3ff98972d
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-02-03 17:39:57 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link id="login" to="/login">
-                        LogIn
-                    </Link>
-                </li>
-                <li>
-                    <Link id="profile" to="/profile">
-                        Profile
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link id="login" to="/login">
+            LogIn
+          </Link>
+        </li>
+        <li>
+          <Link id="profile" to="/profile">
+            Profile
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
